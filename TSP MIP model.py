565e3c08f2ec776f5c51ff07d78bff82ffcdece6
JoepLiefting <joep.liefting@gmail.com>
2022-11-13 10:38:32 +0100

    read_files toegevoegd

---
+++ TSP MIP model.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import vrplot
+import matplotlib.pyplot as plt
+import seaborn as sns
+ses.set_style("whitegrid")
+import numpy as np
+import pandas as pd
+
+n_customers = 10
+figsize = (8, 8)
+
+
+
+depot, customer
